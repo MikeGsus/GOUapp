b240177ebac5c5d3da5400d78c3e2142e3a5fe88
MikeGsus <mike08ahjhotmail.com>
2020-01-13 14:51:09 -0600

    Add redux and persistor to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,26 +8,33 @@ import {
   Link,
   useRouteMatch,
   useParams
-} from "react-router-dom";
+} from 'react-router-dom'
+
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <PersistGate loading={null}>
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className='App-link'
+              href='https://reactjs.org'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </PersistGate>
+    </Provider>
   )
 }
 
